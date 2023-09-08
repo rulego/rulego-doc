d7f8e21d20775aa1223da95213d5b3ea458fb840
rulego-team <rulego@outlook.com>
2023-09-08 18:07:12 +0800

    init

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -28,94 +28,40 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
   themeConfig: {
     // 导航配置
     nav: [
-      { text: '首页', link: '/' },
+      { text: "首页", link: "/" },
       {
-        text: '前端',
-        link: '/web/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+        text: "指南",
+        link: "/pages/0f6af2/",
         items: [
-          // 说明：以下所有link的值只是在相应md文件头部定义的永久链接（不是什么特殊编码）。另外，注意结尾是有斜杠的
-          {
-            text: '前端文章',
-            items: [
-              { text: 'JavaScript', link: '/pages/8143cc480faf9a11/' },
-            ],
-          },
-          {
-            text: '学习笔记',
-            items: [
-              { text: '《JavaScript教程》', link: '/note/javascript/' },
-              { text: '《JavaScript高级程序设计》', link: '/note/js/' },
-              { text: '《ES6 教程》', link: '/note/es6/' },
-              { text: '《Vue》', link: '/note/vue/' },
-              { text: '《React》', link: '/note/react/' },
-              {
-                text: '《TypeScript 从零实现 axios》',
-                link: '/note/typescript-axios/',
-              },
-              {
-                text: '《Git》',
-                link: '/note/git/',
-              },
-              {
-                text: 'TypeScript',
-                link: '/pages/51afd6/',
-              },
-              {
-                text: 'JS设计模式总结',
-                link: '/pages/4643cd/',
-              },
-            ],
-          },
-        ],
+          { text: "快速入门", link: "/pages/0f6af2/" },
+          { text: "核心功能", link: "/pages/779a6e/" },
+          { text: "扩展", link: "/pages/6b03c5/" },
+          { text: "插件", link: "/pages/2976a3/" }
+        ]
       },
       {
-        text: '页面',
-        link: '/ui/',
+        text: "配置",
+        link: "/pages/56bac0/",
         items: [
-          { text: 'HTML', link: '/pages/8309a5b876fc95e3/' },
-          { text: 'CSS', link: '/pages/0a83b083bdf257cb/' },
-        ],
+          { text: "使用配置", link: "/pages/56bac0/" },
+          { text: "代码生成器配置新", link: "/pages/981406/" },
+          { text: "代码生成器配置旧", link: "/pages/061573/" }
+        ]
       },
+      { text: "生态", link: "/pages/24ae02/" },
+      { text: "问答", link: "/pages/f84a74/" },
+      { text: "💖支持", link: "/pages/4fedc2/" },
       {
-        text: '技术',
-        link: '/technology/',
+        text: "🚀低代码平台",
+        link: "/pages/1fedc1/",
         items: [
-          { text: '技术文档', link: '/pages/9a7ee40fc232253e/' },
-          { text: 'GitHub技巧', link: '/pages/4c778760be26d8b3/' },
-          { text: 'Nodejs', link: '/pages/117708e0af7f0bd9/' },
-          { text: '博客搭建', link: '/pages/41f87d890d0a02af/' },
-        ],
+          { text: "数据处理模块", link: "/pages/1fedc1/" },
+          { text: "单体开发平台", link: "/pages/2fedc1/" },
+          { text: "微服务开发平台", link: "/pages/3fedc1/" },
+          { text: "插件式一体化开发平台", link: "/pages/4fedc1/" }
+        ]
       },
-      {
-        text: '更多',
-        link: '/more/',
-        items: [
-          { text: '学习', link: '/pages/f2a556/' },
-          { text: '面试', link: '/pages/aea6571b7a8bae86/' },
-          { text: '心情杂货', link: '/pages/2d615df9a36a98ed/' },
-          { text: '实用技巧', link: '/pages/baaa02/' },
-          { text: '友情链接', link: '/friends/' },
-        ],
-      },
-      { text: '关于', link: '/about/' },
-      {
-        text: '收藏',
-        link: '/pages/beb6c0bd8a66cea6/',
-        // items: [
-        //   { text: '网站', link: '/pages/beb6c0bd8a66cea6/' },
-        //   { text: '资源', link: '/pages/eee83a9211a70f9d/' },
-        //   { text: 'Vue资源', link: '/pages/12df8ace52d493f6/' },
-        // ],
-      },
-      {
-        text: '索引',
-        link: '/archives/',
-        items: [
-          { text: '分类', link: '/categories/' },
-          { text: '标签', link: '/tags/' },
-          { text: '归档', link: '/archives/' },
-        ],
-      },
+      { text: "更新日志", link: "https://github.com/baomidou/mybatis-plus/blob/3.0/CHANGELOG.md" },
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: '/img/logo.png', // 导航栏logo
